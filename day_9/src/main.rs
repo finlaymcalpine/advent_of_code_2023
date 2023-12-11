70900be7b6eb97752ea78abd3c73f6b168dbb228
Finlay McAlpine <finlay.mcalpine1@googlemail.com>
2023-12-10 22:51:10 -0500

    day 9 updated to accept vector as arg

--- day_9/src/main.rs
+++ day_9/src/main.rs
@@ -2,8 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 /*
-When I first wrote this, I was passing a mutable vector to the sequence finder functions, and then returning that vector from the function. I thought that might be a valid way to do it, but it was yielding a result far too high.
-I'm not yet sure why.
+
 */
 
 const FILE: &str = "../inputs/day9_1.txt";
@@ -13,8 +12,8 @@ fn parser(filename: &str) -> BufReader<File> {
     BufReader::new(file)
 }
 
-fn sequence_finder(digits: Vec<isize>) -> Vec<Vec<isize>> {
-    let mut sequences = vec![digits];
+fn sequence_finder(digits: Vec<isize>, sequences: &mut Vec<Vec<isize>>) -> &mut Vec<Vec<isize>> {
+    sequences.push(digits);
 
     while !sequences.last().unwrap().iter().all(|v| *v == 0isize) {
         let diffs: Vec<isize> = sequences
@@ -56,12 +55,14 @@ fn main() {
     let mut sigma2: isize = 0;
 
     for line in file.lines() {
+        let mut sequence = vec![];
         let s = line.unwrap();
         let numbers: Vec<isize> = s
             .split_whitespace()
             .map(|x| x.parse().unwrap())
             .collect();
-        let seq = sequence_finder(numbers);
+        let seq = sequence_finder(numbers, &mut sequence);
+
         sigma += extrapolate(seq.clone());
         sigma2 += extrapolate_back(seq.clone())
     }
